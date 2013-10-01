884bc2f419019b7c3835ac25010993d0ea097ac1
Fidel Sosa <fidel@fidelsosa.com>
2013-10-01 03:44:24 +0000

    added bucket creation by key

--- labs/lab4/data_matcher.rb
+++ labs/lab4/data_matcher.rb
@@ -33,14 +33,43 @@ def scoreMatches (matches, truth)
 	puts "TRUE_POS = #{true_pos}, FALSE_POS = #{false_pos}, PREC = #{precision}, RECALL = #{recall}, F = #{fscore}"
 end
 
+# Approach:
+# 1. Filter down entries to compare by creating a hash of postal codes to entries with the same code
+
+def createBucket (entry_list, key_name)
+	bucket = {}
+	# for each item, check if key exists, if so add to list under that key
+	entry_list.each do |entry|
+		if !entry[key_name].nil?
+			key = entry[key_name]
+			(bucket[key] ||= []) << entry 
+			#	bucket[key] = entry
+			#else
+			#	bucket[key].push(entry)
+		end	
+	end
+
+	return bucket
+end
+
+def findMatches (file_1, file_2)
+	bucket_key_name = "postal_code"
+	first_set = importData(file_1)
+	second_set = importData(file_2)
+
+	first_bucket = createBucket(first_set, bucket_key_name)
+	second_bucket = createBucket(second_set, bucket_key_name)
+end
+
 if ARGV.length != 2
 	puts "NOTE: Requires Ruby 1.9.2+ or the Ruby JSON gem ('gem install json')"
 	puts "USAGE: #{$0} file1.json file2.json"
 end
 
 if ARGV.length > 0
-	importData ARGV[0] 
-	puts "TRUE_POS = #{@true_pos}, FALSE_POS = #{@false_pos}, PREC = #{@precision}, RECALL = #{@recall}, F = #{@fscore}"
+	puts ARGV	
+	findMatches ARGV[0], ARGV[1]
+	#puts "TRUE_POS = #{@true_pos}, FALSE_POS = #{@false_pos}, PREC = #{@precision}, RECALL = #{@recall}, F = #{@fscore}"
 end
 
 
