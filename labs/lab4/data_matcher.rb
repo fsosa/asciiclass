d0b2e881fb6086d4aee0663b370939192237d885
Fidel Sosa <fidel@fidelsosa.com>
2013-10-01 02:57:08 +0000

    finished first draft of scorer

--- labs/lab4/data_matcher.rb
+++ labs/lab4/data_matcher.rb
@@ -12,12 +12,35 @@ def importData (filename)
 end
 
 
+# TODO: FACTOR OUT INTO OWN CLASS
+# Takes a list of [first_id, second_id] pairs and a hash of the ground truth
+def scoreMatches (matches, truth)
+	@true_pos, @true_neg, @false_pos = 0, 0, 0
+	
+	matches.each do |match|
+		first_id = match[0]
+		if (truth.include? first_id && truth[first_id] == match[1]) 
+			true_pos += 1
+		else
+			false_pos += 1
+		end	
+	end
+
+	@precision = true_pos.to_f / (true_pos + false_pos)
+	@recall = true_pos.to_f / truth.length
+	@fscore = (2.0 * precision * recall) / (precision + recall)
+
+	puts "TRUE_POS = #{true_pos}, FALSE_POS = #{false_pos}, PREC = #{precision}, RECALL = #{recall}, F = #{fscore}"
+end
+
 if ARGV.length != 2
+	puts "NOTE: Requires Ruby 1.9.2+ or the Ruby JSON gem ('gem install json')"
 	puts "USAGE: #{$0} file1.json file2.json"
 end
 
 if ARGV.length > 0
 	importData ARGV[0] 
+	puts "TRUE_POS = #{@true_pos}, FALSE_POS = #{@false_pos}, PREC = #{@precision}, RECALL = #{@recall}, F = #{@fscore}"
 end
 
 
