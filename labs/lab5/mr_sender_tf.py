165ada8d43d13e9f6ccbb067da1472c57b6abfa3
Fidel Sosa <fidel@fidelsosa.com>
2013-10-04 05:55:54 +0000

    finished local version but doesn't work on EMR because of the import boto call

--- labs/lab5/mr_sender_tf.py
+++ labs/lab5/mr_sender_tf.py
@@ -1,10 +1,12 @@
 import sys
 import os
 import json
+import StringIO
 from mrjob.protocol import JSONValueProtocol, JSONProtocol
 from mrjob.job import MRJob
+from mrjob.emr import 
 from term_tools import get_terms
-from boto.s3.connection import S3Connection
+
 
 class MRSenderTF(MRJob):
 	INPUT_PROTOCOL = JSONValueProtocol
@@ -15,25 +17,23 @@ class MRSenderTF(MRJob):
 			yield {'sender':email['sender'], 'term':term}, 1
 
 
-	def get_parts_from_s3
-		conn = S3Connection()
-		bucket = conn.get_bucket('6885public')
-		parts = bucket.list('fsosa/tf-idf')
-		return parts		
-	
 	def reducer_init(self):
 		# We want to create an overall dictionary of word, idf pairs 
 		self.per_word_idfs =  {}
+	
+		# Create the S3 connection to get the list of intermediate idf parts	
+		conn = S3Connection()
+		bucket = conn.get_bucket('6885public')
+		idf_parts = bucket.list('fsosa/term-idfs/part') # Ignore that _SUCCESS file 
 
 		# Iterate over all the parts to build the entire word dictionary
-		idf_parts = os.listdir(per_term_dir)
-		for parts in idf_parts:
-			path = per_term_dir + "/" + parts
-			part_file = open(path, 'r')
-			for line in part_file:
-				word_idf = json.loads(line)
-				word = word_idf["term"]
-				idf = word_idf["idf"]
+		for part in idf_parts:
+			part_string = part.get_contents_as_string()
+			io = StringIO.StringIO(part_string)
+			for line in io:
+				pair = json.loads(line)
+				word = pair["term"]
+				idf = pair["idf"]
 
 				self.per_word_idfs[word] = idf	
 
@@ -42,7 +42,8 @@ class MRSenderTF(MRJob):
 		word = term['term']
 		sender = term['sender']
 		per_word_idf = self.per_word_idfs[word]
-		
+	
+		# Calculate the final TD-IDF	
 		final_tf_idf = sender_tf * per_word_idf
 		yield sender, {'word':word, 'tf-idf':final_tf_idf}
 
